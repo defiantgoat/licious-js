049546042b95625aca54af307f7a0ccdb56bfc33
Jayson Ward <defiantgoat@gmail.com>
2022-11-19 15:17:58 -0500

    added storybook

---
+++ licious-js/src/components/licious-button/licious-button.stories.ts
@@ -0,0 +1,33 @@
+export default {
+  // this creates a ‘Components’ folder and a ‘MyComponent’ subfolder
+  title: 'Licioius/LiciousButton',
+  argTypes: {
+    icon: {
+      options: ["paint", "menu", "trash","save" ,"rubbish" ,"edit" , "close" , "logout"],
+      control: { type: 'select' }, // Automatically inferred when 'options' is defined
+    },
+    size: {
+      options: ['lg', 'sm',],
+      control: { type: 'select' }, // Automatically inferred when 'options' is defined
+    },
+  },
+};
+
+const Template = (args) => `<div style="width:${args.width}"><licious-button icon="${args.icon}" size="${args.size}" label="${args.label}" disabled="${args.disabled}"></licious-button></div>`;
+
+export const Large = Template.bind({});
+Large.args = {
+  width: '200px',
+  icon: 'paint',
+  size: 'lg',
+  label: 'Paint',
+  disabled: false
+};
+export const Small = Template.bind({});
+Small.args = {
+  width: '120px',
+  icon: 'paint',
+  size: 'sm',
+  label: 'Paint',
+  disabled: false
+};
